b59bac733c9cd507c5d6c2e2d119e11c7bc3b173
AlphaTr <trefoiles@gmail.com>
2015-01-09 12:36:36 +0800

    move useso.com to lug.ustc.edu.cn

--- readme.md
+++ readme.md
@@ -1,9 +1,17 @@
 ### ReadMe
 =================
 
-A Chrome extension, to redirect google fonts to useso.com
+一个重定向 Google 静态文件到 USTC 服务器的 Chrome 插件
+A Chrome extension, to redirect google static to lug.ustc.edu.cn
 
-[http://libs.useso.com/](useso.com)
+[https://servers.ustclug.org/2014/07/ustc-blog-force-google-fonts-proxy/](ustclug.org)
+
+会使用 Chrome 内部机制，对下面的域名做 307 内部跳转
+
+- fonts.googleapis.com > fonts.lug.ustc.edu.cn
+- ajax.googleapis.com > ajax.lug.ustc.edu.cn
+- themes.googleusercontent.com > google-themes.lug.ustc.edu.cn
+- fonts.gstatic.com > fonts-gstatic.lug.ustc.edu.cn
 
 ## install
 
--- src/main.js
+++ src/main.js
@@ -4,24 +4,29 @@
     chrome.webRequest.onBeforeRequest.addListener(function (details) {
         var redirectUrl = '';
         if (details.url.indexOf('fonts.googleapis.com') !== -1) {
-            redirectUrl = details.url.replace(/\/\/fonts\.googleapis\.com/, '//fonts.useso.com');
+            redirectUrl = details.url.replace(/\/\/fonts\.googleapis\.com/, '//fonts.lug.ustc.edu.cn');
             return {redirectUrl: redirectUrl};
         }
         if (details.url.indexOf('ajax.googleapis.com') !== -1) {
-            redirectUrl = details.url.replace(/\/\/ajax\.googleapis\.com/, '//ajax.useso.com');
+            redirectUrl = details.url.replace(/\/\/ajax\.googleapis\.com/, '//ajax.lug.ustc.edu.cn');
             return {redirectUrl: redirectUrl};
         }
         if (details.url.indexOf('themes.googleusercontent.com') !== -1) {
-            redirectUrl = details.url.replace(/\/\/themes\.googleusercontent\.com/, '//fontscontent.useso.com');
+            redirectUrl = details.url.replace(/\/\/themes\.googleusercontent\.com/, '//google-themes.lug.ustc.edu.cn');
+            return {redirectUrl: redirectUrl};
+        }
+        if (details.url.indexOf('fonts.gstatic.com') !== -1) {
+            redirectUrl = details.url.replace(/\/\/fonts\.gstatic\.com/, '//fonts-gstatic.lug.ustc.edu.cn');
             return {redirectUrl: redirectUrl};
         }
     },
     {
         /* useso 暂时只支持 http 协议，所以这里先做 http 协议的 */
         urls: [
-            'http://fonts.googleapis.com/*',
-            'http://ajax.googleapis.com/*',
-            'http://themes.googleusercontent.com/static/fonts/*'
+            '*://fonts.googleapis.com/*',
+            '*://ajax.googleapis.com/*',
+            '*://themes.googleusercontent.com/static/fonts/*',
+            '*://fonts.gstatic.com/*',
         ]
     },
     ['blocking']);
